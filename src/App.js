90502ab24c0f6b4fdd201236cbf0183bed4a6195
Dani G <twooctrox@gmail.com>
2020-10-19 12:10:49 -0400

    updated app to redirect to home if user is signed in. Fixed error in app.js that prevented user from sigining out.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
-import './pages/homepage/homepage.styles.scss';
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import Header from "./components/header/header.component";
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-    unsubscribeFromAuth = null
+    unsubscribeFromAuth = null;
 
     componentDidMount() {
         const { setCurrentUser } = this.props;
@@ -22,15 +21,15 @@ class App extends React.Component {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
 
-                userRef.onSnapshot(snapshot => {
+                userRef.onSnapshot(snapShot => {
                     setCurrentUser({
-                        id: snapshot.id,
-                        ...snapshot.data()
+                        id: snapShot.id,
+                        ...snapShot.data()
                     });
                 });
             }
 
-            setCurrentUser({currentUser: userAuth});
+            setCurrentUser(userAuth);
         });
     }
 
@@ -39,21 +38,38 @@ class App extends React.Component {
     }
 
     render() {
-      return (
-          <div>
-              <Header />
-              <Switch>
-                  <Route exact path='/' component={HomePage}/>
-                  <Route path='/shop' component={ShopPage}/>
-                  <Route path='/signin' component={SignInAndSignUpPage} />
-              </Switch>
-          </div>
-      );
-  }
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route
+                        exact
+                        path='/signin'
+                        render={() =>
+                            this.props.currentUser ? (
+                                <Redirect to='/' />
+                            ) : (
+                                <SignInAndSignUpPage />
+                            )
+                        }
+                    />
+                </Switch>
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import SignIn from "../../components/sign-in/sign-in.componnent";
-import SignUp from "../../components/sign-up/sign-up.component";
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
-
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
