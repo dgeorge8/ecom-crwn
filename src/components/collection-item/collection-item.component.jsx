cc83090a8fc31d044cd33b04e6f1cc4c7246ca6a
Dani G <twooctrox@gmail.com>
2020-11-04 10:37:46 -0500

    moved directory and shop into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from "../custom-button/custom-button.component";
-import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({item, addItem}) => {
+const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
 
-    return(
+    return (
         <div className='collection-item'>
             <div
                 className='image'
@@ -18,18 +18,21 @@ const CollectionItem = ({item, addItem}) => {
                 }}
             />
             <div className='collection-footer'>
-                <span className='name'>{ name }</span>
-                <span className='price'>{ price }</span>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
             </div>
             <CustomButton onClick={() => addItem(item)} inverted>
-                Add to Cart
+                Add to cart
             </CustomButton>
         </div>
     );
-}
+};
 
 const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
-})
+});
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(
+    null,
+    mapDispatchToProps
+)(CollectionItem);
